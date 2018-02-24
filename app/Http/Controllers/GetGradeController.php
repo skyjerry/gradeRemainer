90e7fdcfa71ce471ce5425773ce6f61014da16ca
skyjerry <ldwangzhen@gmail.com>
2018-02-24 16:02:30 +0800

    update readme.md

--- app/Http/Controllers/GetGradeController.php
+++ app/Http/Controllers/GetGradeController.php
@@ -12,9 +12,6 @@ class GetGradeController extends Controller
     protected $queryList;
     protected $users;
 
-    protected const URL = 'http://www.jwc.ldu.edu.cn/cj/';
-    protected const URL_POST = 'http://www.jwc.ldu.edu.cn/cj/chaxun.asp';
-
     public function __construct(QueryList $queryList, Users $users)
     {
         $this->queryList = $queryList;
@@ -24,26 +21,5 @@ class GetGradeController extends Controller
     public function index()
     {
 
-        $student_id = '20152203085';
-        $name = '王振';
-        $password = '19980428';
-        $phone  = '13031639553';
-
-        //dd($grades);
-//        $data_array = [
-//            'student_id' => $student_id,
-//            'password' => $password,
-//            'name' => $name,
-//            'grade' => json_encode($grades),
-//            'phone' => $phone
-//        ];
-//        $users = new Users();
-//        $res = $users->create($data_array);
-//        dd($res);
-
-//        $c = $grades;
-//        $c['编译原理'] = '99';
-//        $arr = array_diff_assoc($grades, $c);
-//        dd($arr);
     }
 }
